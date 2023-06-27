74cbf6d3a8eab7d2bb3042ab491a24f39cc2c07b
gari8641 <gari8641@gmail.com>
2023-06-27 11:18:01 +0900

    update: フォームを設置した。ファビコンとバナーも設置した。
    フォーム：入力された数字にランダムで-1から6のいずれかを掛け算する。
    バナーとfavicon：バナーは修正かな。。。
    
    https://github.com/users/gari8641/projects/10/views/1?pane=issue&itemId=31807361

--- app/Http/Controllers/NumberController.php
+++ app/Http/Controllers/NumberController.php
@@ -7,4 +7,18 @@ use Illuminate\Http\Request;
 class NumberController extends Controller
 {
     //
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function show(Request $request)
+    {
+        $number = $request->input('number');
+
+        //ランダムな数をかける
+        $number = $number * rand(-1,6);
+
+        return view('index', compact('number'));
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,36 +2,26 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title></title>
-
+  <title>ゲーミフィケーター</title>
+    <link rel="shortcut icon" href="{{ asset('/images/favicon.ico') }}">
 <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 <body>
 
-次は、フォーム作成から。入力した数字を受け取れるようにする。コントローラで
+
+<img src="images/moti-logo.png">
 
 
-    <a href="#" class="inline-block bg-red-200 hover:bg-red-400 py-2 px-6 rounded-full shadow-md">
-      ボタン1
-    </a>
 
-<p>
-合計
-</p>
+    <form action="{{ route('number.show') }}" method="post" class="inline-block bg-red-200 hover:bg-red-400 py-2 px-6 rounded-full shadow-md">
+        @csrf
+        <input type="number" name="number">
+        <button type="submit">Submit</button>
+    </form>
 
-<p>
-入力する文字
-
-結果
-</p>
-
-<p>
-ボタン
-</p>
-
-  {{ $user }} <br>
-  {{ $score }}
-
+    @if(isset($number))
+        <h2>Entered Number: {{ $number }}</h2>
+    @endif
 </body>
 </html>
