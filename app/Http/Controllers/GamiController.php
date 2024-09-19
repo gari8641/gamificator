e217c5983d7f0ccba8c7823cc4219fdb380af563
宮田くん <hidenobu@bitpark.co.jp>
2024-09-19 09:37:22 +0900

    update: 合計点を取得するようにした。＆ロゴを非表示にした。

--- app/Http/Controllers/GamiController.php
+++ app/Http/Controllers/GamiController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// ログインユーザー取得のため
+use Illuminate\Support\Facades\Auth;
+//use App\Models\Point;
+use App\Point;
+
+
+
 class GamiController extends Controller
 {
     //
   public function index()
   {
-    $user = 'ほげ';
-    $score = '点数';
 
-    return view('index', compact('user', 'score'));
+        // 現在ログインしているユーザーのIDを取得
+        $user_id = Auth::id();
+
+        // ユーザーのポイントを合計
+        $score = $user_id ? Point::where('user_id', $user_id)->sum('point') : 0;
+
+        return view('index', compact('user_name', 'score'));
   }
+
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,22 @@
 @section('content')
 
 <div class="flex flex-col justify-center items-center">
-<div class="w-full flex justify-end">
-    <img src="images/moti-logo2.png">
-</div>
+    <!-- <div class="w-full flex justify-end">
+        <img src="images/moti-logo2.png">
+    </div> -->
     <form action="{{ route('number.show') }}" method="post" class="inline-block bg-red-200 hover:bg-red-400 py-2 px-6 rounded-full shadow-md">
         @csrf
         <input type="number" name="number">
         <button type="submit">Submit</button>
     </form>
 
+    <h2>合計ポイント: {{ $score ?? 'ポイントが取得できませんでした' }}</h2>
+
+
     @if(isset($number))
         <h2>Entered Number: {{ $number }}</h2>
     @endif
+
 
 </div>
 @endsection
