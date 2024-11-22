8c1c0b5e5bd0f784d32609179f7f03adb7200181
宮田くん <hidenobu@bitpark.co.jp>
2024-11-22 11:34:00 +0900

    add: 30ポイントボタン作成。js埋め込みだから後で動かすかも

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,22 @@
     <!-- <div class="w-full flex justify-end">
         <img src="images/moti-logo2.png">
     </div> -->
-    <form action="{{ route('number.show') }}" method="post" class="inline-block bg-red-200 hover:bg-red-400 py-2 px-6 rounded-full shadow-md">
-        @csrf
-        <input type="number" name="number" value="50">
-        <button type="submit">Submit</button>
-    </form>
+
+    <div class="flex space-x-4">
+
+        <button type="button" class="bg-blue-200 hover:bg-blue-400 py-2 px-6 rounded-full shadow-md mt-2" onclick="setNumber(30)">
+            30
+        </button>
+        <button type="button" class="bg-blue-200 hover:bg-blue-400 py-2 px-6 rounded-full shadow-md mt-2" onclick="setNumber(50)">
+            50
+        </button>
+    </div>
+        
+        <form action="{{ route('number.show') }}" method="post" class="inline-block bg-red-200 hover:bg-red-400 py-2 px-6 rounded-full shadow-md">
+            @csrf
+            <input id="numberinput" type="number" name="number" value="50">
+            <button type="submit">Submit</button>
+        </form>
 
 
 
@@ -23,4 +34,9 @@
     @endauth
 
 </div>
+<script>
+    function setNumber(value) {
+        document.getElementById('numberinput').value = value;
+    }
+</script>
 @endsection
